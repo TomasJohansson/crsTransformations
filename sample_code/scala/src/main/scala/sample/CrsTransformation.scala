2d2e431c2422d7dc948a18b32890c4a4ec1068b8
Tomas Johansson <github-tomas@programmerare.com>
2020-04-13 01:21:32 +0200

    Implemented sample code with Scala. One object with a main method and one test class. Both are doing the same transformation but the main method prints output to the screen while the test method executes test assertions. They can be run with SBT, i.e. "sbt run" and "sbt test"

--- sample_code/scala/src/main/scala/sample/CrsTransformation.scala
+++ sample_code/scala/src/main/scala/sample/CrsTransformation.scala
@@ -3,10 +3,100 @@ package sample
 // the package below is from the Java library "crs-transformation-constants"
 import com.programmerare.crsConstants.constantsByAreaNameNumber.v9_5_4.EpsgNumber
 
-// cd sample_code/scala
-// sbt run
+// the other "com.programmerare" packages below are from Kotlin libraries ( artifactId's starting with "crs-transformation-adapter-" , https://mvnrepository.com/artifact/com.programmerare.crs-transformation )
+import com.programmerare.crsTransformations.coordinate.{CrsCoordinate, CrsCoordinateFactory}
+import com.programmerare.crsTransformations.crsIdentifier.{CrsIdentifier, CrsIdentifierFactory}
+import com.programmerare.crsTransformations.{CrsTransformationAdapter, CrsTransformationResult}
+import com.programmerare.crsTransformations.compositeTransformations.CrsTransformationAdapterCompositeFactory
+import com.programmerare.crsTransformations.compositeTransformations.CrsTransformationAdapterWeight.createFromInstance
+import com.programmerare.crsTransformationAdapterProj4J.CrsTransformationAdapterProj4J
+import com.programmerare.crsTransformationAdapterGeoTools.CrsTransformationAdapterGeoTools
+import com.programmerare.crsTransformationAdapterGooberCTL.CrsTransformationAdapterGooberCTL
+import com.programmerare.crsTransformationAdapterOrbisgisCTS.CrsTransformationAdapterOrbisgisCTS
+import com.programmerare.crsTransformationAdapterGeoPackageNGA.CrsTransformationAdapterGeoPackageNGA
+
+import scala.jdk.CollectionConverters._ // https://docs.scala-lang.org/overviews/collections-2.13/conversions-between-java-and-scala-collections.html
+
 object CrsTransformation {
+
+  // cd sample_code/scala
+  // sbt run
   def main(args: Array[String]): Unit = {
-    println("EpsgNumber.SWEDEN__SWEREF99_TM__3006 " + EpsgNumber.SWEDEN__SWEREF99_TM__3006)    
+    println(s"Implicit/default EpsgNumber for latitude/longitude: ${inputCoordinate.getCrsIdentifier.getEpsgNumber}") // 4326
+    println(s"InputCoordinate: ${inputCoordinate}")
+    println(s"TargetCrsIdentifier: ${targetCrsIdentifier}")
+    println(s"Coordinate transformations from EPSG ${EpsgNumber.WORLD__WGS_84__4326} to EPSG ${EpsgNumber.SWEDEN__SWEREF99_TM__3006}")
+
+    val crsTransformationResults = transform(inputCoordinate, targetCrsIdentifier, getAllCrsTransformationAdapters())
+    crsTransformationResults.foreach { result =>
+      val nameOfCrsTransformationAdapterImplementation = result.getCrsTransformationAdapterResultSource.getAdapteeType
+      val outputCoordinate = result.getOutputCoordinate
+      println(s"$nameOfCrsTransformationAdapterImplementation : X / Y ===> ${outputCoordinate.getX} / ${outputCoordinate.getX}")
+    }
+    
+    // Below is the printed output from the above code in this main method: 
+    
+    //  Implicit/default EpsgNumber for latitude/longitude: 4326
+    //  InputCoordinate: Coordinate(xEastingLongitude=18.059196, yNorthingLatitude=59.330231, crsIdentifier=CrsIdentifier(crsCode='EPSG:4326', isEpsgCode=true, epsgNumber=4326))
+    //  TargetCrsIdentifier: CrsIdentifier(crsCode='EPSG:3006', isEpsgCode=true, epsgNumber=3006)
+    //  Coordinate transformations from EPSG 4326 to EPSG 3006
+    //  LEAF_PROJ4J_0_1_0 : X / Y ===> 674032.357326444 / 674032.357326444
+    //  LEAF_NGA_GEOPACKAGE_3_1_0 : X / Y ===> 674032.357326444 / 674032.357326444
+    //  LEAF_ORBISGIS_1_5_1 : X / Y ===> 674032.3573261796 / 674032.3573261796
+    //  LEAF_GOOBER_1_1 : X / Y ===> 674032.357 / 674032.357
+    //  LEAF_GEOTOOLS_20_0 : X / Y ===> 674032.3571771547 / 674032.3571771547
+    //  COMPOSITE_MEDIAN : X / Y ===> 674032.3573261796 / 674032.3573261796
+    //  COMPOSITE_AVERAGE : X / Y ===> 674032.3572312444 / 674032.3572312444
+    //  COMPOSITE_FIRST_SUCCESS : X / Y ===> 674032.357 / 674032.357
+    //  COMPOSITE_WEIGHTED_AVERAGE : X / Y ===> 674032.3571927036 / 674032.3571927036
+  }
+  
+  private lazy val inputCoordinate = CrsCoordinateFactory.latLon(59.330231, 18.059196) //  Implicit/Default CrsIdentifier for latitude/longitude: EpsgNumber.WORLD__WGS_84__4326
+  private lazy val targetCrsIdentifier = CrsIdentifierFactory.createFromEpsgNumber(EpsgNumber.SWEDEN__SWEREF99_TM__3006)
+  private lazy val weightedAverageCrsTransformationAdapter = {
+    val crsTransformationAdapterWeights = List(
+      createFromInstance(new CrsTransformationAdapterProj4J, 1.0),
+      createFromInstance(new CrsTransformationAdapterOrbisgisCTS, 1.0),
+      createFromInstance(new CrsTransformationAdapterGeoPackageNGA, 1.0),
+      createFromInstance(new CrsTransformationAdapterGeoTools, 1.0),
+      createFromInstance(new CrsTransformationAdapterGooberCTL, 2.0)
+    )
+    CrsTransformationAdapterCompositeFactory.createCrsTransformationWeightedAverage(
+      crsTransformationAdapterWeights.asJava // "asJava" is available thanks to:  import scala.jdk.CollectionConverters._
+    )
+  }
+
+  private lazy val allCrsTransformationAdapters = List(
+    new CrsTransformationAdapterProj4J,
+    new CrsTransformationAdapterGeoPackageNGA,
+    new CrsTransformationAdapterOrbisgisCTS,
+    new CrsTransformationAdapterGooberCTL,
+    new CrsTransformationAdapterGeoTools,
+    CrsTransformationAdapterCompositeFactory.createCrsTransformationMedian(),
+    CrsTransformationAdapterCompositeFactory.createCrsTransformationAverage(),
+    CrsTransformationAdapterCompositeFactory.createCrsTransformationFirstSuccess(),
+    weightedAverageCrsTransformationAdapter
+  )
+
+  def getTargetCrsIdentifier(): CrsIdentifier = {
+    targetCrsIdentifier
+  }
+
+  def getInputCoordinate(): CrsCoordinate = {
+    inputCoordinate
+  }
+
+  def getAllCrsTransformationAdapters(): List[CrsTransformationAdapter] = {
+    allCrsTransformationAdapters
+  }
+
+  def transform(
+    inputCoordinate: CrsCoordinate,
+    targetCrsIdentifier: CrsIdentifier,
+    allCrsTransformationAdapters: List[CrsTransformationAdapter]
+  ): List[CrsTransformationResult] = {
+    allCrsTransformationAdapters.map { crsTransformationAdapter =>
+      crsTransformationAdapter.transform(inputCoordinate, targetCrsIdentifier)
+    }
   }
 }
--- sample_code/scala/src/test/scala/sample/CrsTransformationTest.scala
+++ sample_code/scala/src/test/scala/sample/CrsTransformationTest.scala
@@ -1,42 +1,65 @@
 package sample
 
+import org.scalatest.matchers.should.Matchers._
 import org.scalatest.funsuite.AnyFunSuite
 
 // the import below is from the Java library "crs-transformation-constants"
 import com.programmerare.crsConstants.constantsByAreaNameNumber.v9_5_4.EpsgNumber
 
-// the two imports below are from the core Kotlin library "crs-transformation-adapter-core"
+// the three imports below are from the core Kotlin library "crs-transformation-adapter-core"
 import com.programmerare.crsTransformations.coordinate.CrsCoordinate
 import com.programmerare.crsTransformations.coordinate.CrsCoordinateFactory
-
-// the imports below are from Kotlin adapter implementation libraries
-import com.programmerare.crsTransformationAdapterProj4J.CrsTransformationAdapterProj4J
-import com.programmerare.crsTransformationAdapterGeoPackageNGA.CrsTransformationAdapterGeoPackageNGA
-import com.programmerare.crsTransformationAdapterOrbisgisCTS.CrsTransformationAdapterOrbisgisCTS
-import com.programmerare.crsTransformationAdapterGooberCTL.CrsTransformationAdapterGooberCTL
-import com.programmerare.crsTransformationAdapterGeoTools.CrsTransformationAdapterGeoTools
+import com.programmerare.crsTransformations.CrsTransformationResult
 
 // cd sample_code/scala
 // sbt test
 class CrsTransformationTest extends AnyFunSuite {
-  
-  // VERY basic "test" below just to show that the Java and Kotlin types (retrieved with SBT for Scala)
-  // can be referred to from this Scala code  
-  
-  test("Java and Kotlin types available") {
-    
-    assert(EpsgNumber.SWEDEN__SWEREF99_TM__3006 === 3006)
-    
-    // the above constant class is implemented with Java while the below types are implemented with Kotlin  
 
-    new CrsTransformationAdapterProj4J
-    new CrsTransformationAdapterGeoPackageNGA
-    new CrsTransformationAdapterOrbisgisCTS
-    new CrsTransformationAdapterGooberCTL
-    new CrsTransformationAdapterGeoTools
+  test("Transform coordinates from EPSG 4326 to EPSG 3006 with all CrsTransformationAdapters") {
+    val allCrsTransformationAdapters = CrsTransformation.getAllCrsTransformationAdapters()
+    val inputCoordinate = CrsTransformation.getInputCoordinate()
+    val targetCrsIdentifier = CrsTransformation.getTargetCrsIdentifier()
 
-    val coordinate: CrsCoordinate = CrsCoordinateFactory.latLon(59.330231, 18.059196)
+    // Transformation from EPSG 4326 to EPSG 3006:
+    assert(inputCoordinate.getCrsIdentifier.getEpsgNumber === EpsgNumber.WORLD__WGS_84__4326) // 4326 is implicit/default EPSG for latitude/longitude
+    assert(targetCrsIdentifier.getEpsgNumber === EpsgNumber.SWEDEN__SWEREF99_TM__3006)
+
+    // The transformations with all 9 crsTransformationAdapters
+    
+    val expectedNumberOfCrsTransformationAdapters = 9
+    
+    assertResult(expectedNumberOfCrsTransformationAdapters)(allCrsTransformationAdapters.size)
+
+    val results = CrsTransformation.transform(inputCoordinate, targetCrsIdentifier, allCrsTransformationAdapters)
+    assertResult(expectedNumberOfCrsTransformationAdapters)(results.size)
+    
+    results.foreach { result =>
+      assertTransformationResult(result)
+    }
+
+  }
+
+  private lazy val expectedOnputCoordinate: CrsCoordinate = CrsCoordinateFactory.yx(6580822.0, 674032.0, EpsgNumber.SWEDEN__SWEREF99_TM__3006)
+
+  private def assertTransformationResult(result: CrsTransformationResult) {
+    val errorMessageWithInformationAboutFailingImplementation = s" Failing implementation: ${result.getCrsTransformationAdapterResultSource.getAdapteeType}"
+
+    assertResult(true, errorMessageWithInformationAboutFailingImplementation) {
+      result.isSuccess
+    }
+
+    val outputCoordinate = result.getOutputCoordinate
+    assertResult(EpsgNumber.SWEDEN__SWEREF99_TM__3006, errorMessageWithInformationAboutFailingImplementation) {
+      outputCoordinate.getCrsIdentifier.getEpsgNumber
+    }
+
+    withClue(errorMessageWithInformationAboutFailingImplementation){
+      outputCoordinate.getX shouldEqual expectedOnputCoordinate.getX +- 0.5
+    }
+
+    withClue(errorMessageWithInformationAboutFailingImplementation){
+      outputCoordinate.getY shouldEqual expectedOnputCoordinate.getY +- 0.5
+    }
   }
   
-  // TODO some real test with coordinate transformation 
 }
