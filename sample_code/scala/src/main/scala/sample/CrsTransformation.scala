75b99abfdb0e9bc5a7f04a4e315cdf2feee7ee4c
Tomas Johansson <github-tomas@programmerare.com>
2020-04-13 15:44:33 +0200

    Correction of Scala sample code. Now also printing the Y values to the screen, which was intended.

--- sample_code/scala/src/main/scala/sample/CrsTransformation.scala
+++ sample_code/scala/src/main/scala/sample/CrsTransformation.scala
@@ -31,24 +31,27 @@ object CrsTransformation {
     crsTransformationResults.foreach { result =>
       val nameOfCrsTransformationAdapterImplementation = result.getCrsTransformationAdapterResultSource.getAdapteeType
       val outputCoordinate = result.getOutputCoordinate
-      println(s"$nameOfCrsTransformationAdapterImplementation : X / Y ===> ${outputCoordinate.getX} / ${outputCoordinate.getX}")
+      println(s"$nameOfCrsTransformationAdapterImplementation : X / Y ===> ${outputCoordinate.getX} / ${outputCoordinate.getY}") 
     }
     
     // Below is the printed output from the above code in this main method: 
-    
+
     //  Implicit/default EpsgNumber for latitude/longitude: 4326
     //  InputCoordinate: Coordinate(xEastingLongitude=18.059196, yNorthingLatitude=59.330231, crsIdentifier=CrsIdentifier(crsCode='EPSG:4326', isEpsgCode=true, epsgNumber=4326))
     //  TargetCrsIdentifier: CrsIdentifier(crsCode='EPSG:3006', isEpsgCode=true, epsgNumber=3006)
     //  Coordinate transformations from EPSG 4326 to EPSG 3006
-    //  LEAF_PROJ4J_0_1_0 : X / Y ===> 674032.357326444 / 674032.357326444
-    //  LEAF_NGA_GEOPACKAGE_3_1_0 : X / Y ===> 674032.357326444 / 674032.357326444
-    //  LEAF_ORBISGIS_1_5_1 : X / Y ===> 674032.3573261796 / 674032.3573261796
-    //  LEAF_GOOBER_1_1 : X / Y ===> 674032.357 / 674032.357
-    //  LEAF_GEOTOOLS_20_0 : X / Y ===> 674032.3571771547 / 674032.3571771547
-    //  COMPOSITE_MEDIAN : X / Y ===> 674032.3573261796 / 674032.3573261796
-    //  COMPOSITE_AVERAGE : X / Y ===> 674032.3572312444 / 674032.3572312444
-    //  COMPOSITE_FIRST_SUCCESS : X / Y ===> 674032.357 / 674032.357
-    //  COMPOSITE_WEIGHTED_AVERAGE : X / Y ===> 674032.3571927036 / 674032.3571927036
+    //  LEAF_PROJ4J_0_1_0 : X / Y ===> 674032.357326444 / 6580821.991123579
+    //  LEAF_NGA_GEOPACKAGE_3_1_0 : X / Y ===> 674032.357326444 / 6580821.991123579
+    //  LEAF_ORBISGIS_1_5_1 : X / Y ===> 674032.3573261796 / 6580821.991121078
+    //  LEAF_GOOBER_1_1 : X / Y ===> 674032.357 / 6580821.991
+    //  LEAF_GEOTOOLS_20_0 : X / Y ===> 674032.3571771547 / 6580821.994371211
+    //  COMPOSITE_MEDIAN : X / Y ===> 674032.3573261796 / 6580821.991123579
+    //  COMPOSITE_AVERAGE : X / Y ===> 674032.3572312444 / 6580821.991747889
+    //  COMPOSITE_FIRST_SUCCESS : X / Y ===> 674032.357 / 6580821.991
+    //  COMPOSITE_WEIGHTED_AVERAGE : X / Y ===> 674032.3571927036 / 6580821.991623241
+
+    Process finished with exit code 0
+
   }
   
   private lazy val inputCoordinate = CrsCoordinateFactory.latLon(59.330231, 18.059196) //  Implicit/Default CrsIdentifier for latitude/longitude: EpsgNumber.WORLD__WGS_84__4326
