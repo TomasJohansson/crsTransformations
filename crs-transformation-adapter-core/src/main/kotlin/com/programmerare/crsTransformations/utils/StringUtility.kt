54fabad1a353e32c91d44960dff35b4b54f3abdf
Tomas Johansson <github-tomas@programmerare.com>
2020-04-20 21:50:32 +0200

    Refactoring. Moved a regular expression to outside of the method in the Kotlin singleton object to avoid creating it more than once. Also some code formatting and variable renaming.

--- crs-transformation-adapter-core/src/main/kotlin/com/programmerare/crsTransformations/utils/StringUtility.kt
+++ crs-transformation-adapter-core/src/main/kotlin/com/programmerare/crsTransformations/utils/StringUtility.kt
@@ -5,6 +5,14 @@ import java.lang.RuntimeException
 internal object StringUtility {
     
     private val regularExpressionForExtractionOfLastVersionNumberString = Regex("""^.*?((\d|\d\.)*\d)+[^\d]*$""")
+
+    /**
+     * The purpose of this regular expression is to use it for extracting the name of a jar file 
+     * from an input string that might include the full "URL" path.
+     * Example: Input string "C:/...6d0/junit-jupiter-api-5.6.2.jar!/org/junit..."
+     * Expected string to return with above input: "junit-jupiter-api-5.6.2.jar"
+     */
+    private val regularExpressionForExtractingTheNameOfJarFileFromUrlPath = Regex("""^.*/(.+?.jar)!.*$""")
     
     val returnValueIfNoNumericalValueIsFound = "?"
     
@@ -20,15 +28,20 @@ internal object StringUtility {
         return matchGroupCollection.get(1)?.value!!
     }
 
+    /**
+     * @param urlPathForJarFile some path string returned from methods such as URL.toExternalForm() or URL.getLocation().toExternalForm()
+     */
     @JvmStatic
-    fun getJarFileNameWithoutThePath(pathForJarFile: String): String {
+    fun getJarFileNameWithoutThePath(urlPathForJarFile: String): String {
         try {
-            val pathWithoutBackslashes = pathForJarFile.replace('\\', '/')
-            val regex = """^.*/(.+?.jar)!.*$""".toRegex()
+            val pathWithoutBackslashes = urlPathForJarFile.replace('\\', '/')
             // the path string could be "... 6d0/junit-jupiter-api-5.6.2.jar!/org/jun ..."
             // i.e. normally something like that above but below is a simple way to make the method        
             // be able to handle e.g. "junit-jupiter-api-5.6.2.jar" by adding the "/" and "!" as prefix and suffix below          
-            val matchResult = regex.find("/" + pathWithoutBackslashes + "!", 0)
+            val matchResult = regularExpressionForExtractingTheNameOfJarFileFromUrlPath.find(
+                "/" + pathWithoutBackslashes + "!",
+                0 // startindex
+            )
             val groupValues = matchResult!!.groupValues
             val jarFileName = groupValues.get(1).trim()
             sanityCheck(jarFileName)
